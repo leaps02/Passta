577dbb2bcd64790b07f6c3067333537e6d595bae
leaps02 <leepakshiunimelb@gmail.com>
2026-02-18 18:12:04 +1100

    Add files via upload

---
+++ ai_studio_code.js
@@ -0,0 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "YOUR_API_KEY",
+    projectId: "YOUR_PROJECT_ID",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+});
+
+const messaging = firebase.messaging();
+
+// Handle background notifications
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
